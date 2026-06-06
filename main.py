003f085d014affce20c26f3a1225fecf1ea42045
Sanket Singh <23f3000872@ds.study.iitm.ac.in>
2026-06-06 15:19:26 +0530

    Student API

---
+++ main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI, Query
+from fastapi.middleware.cors import CORSMiddleware
+import pandas as pd
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["GET"],
+    allow_headers=["*"],
+)
+
+df = pd.read_csv("q-fastapi.csv")
+
+@app.get("/api")
+def get_students(class_: list[str] | None = Query(None, alias="class")):
+    data = df
+
+    if class_:
+        data = data[data["class"].isin(class_)]
+
+    return {
+        "students": data.to_dict(orient="records")
+    }
+
+@app.get("/")
+def root():
+    return {"message": "Student API Running"}
